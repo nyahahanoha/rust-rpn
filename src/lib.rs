a3e2b214fa4bab3d665ab465df77481767b75285
nyahahanoha <rintarooono@gmail.com>
2023-12-06 12:53:20 +0900

    set_operator function is changed

--- src/lib.rs
+++ src/lib.rs
@@ -38,9 +38,21 @@ impl RPN {
         self.operator = Operation::NONE;
     }
 
-    fn set_operator(&mut self, o: Operation) {
+    fn set_operator(&mut self, o: char) {
         if self.is_none_operator() {
-            self.operator = o;
+            if o == '+' {
+                self.operator = Operation::ADD;
+            } else if o == '-' {
+                self.operator = Operation::SUB;
+            } else if o == '*' {
+                self.operator = Operation::MUL;
+            } else if o == '/' {
+                self.operator = Operation::DIV;
+            } else {
+                panic!("Don't know this Operation");
+            }
+        } else {
+            panic!("Operator has been set to Operation");
         }
     }
 
@@ -58,8 +70,9 @@ impl RPN {
 
     fn execute<'a>(&mut self) -> Result<f32, String> {
         if self.is_none_operator() {
-            return Err("Not set operator".to_string());
+            panic!("Not set operator");
         } else if self.len() < 2 {
+            self.delete_operator();
             return Err("Stack is shortage".to_string());
         }
         let a: f32 = self.pop()?;
@@ -126,33 +139,48 @@ mod tests {
     fn test_change_operator(fixture: RPN) {
         let mut a = fixture.clone();
         assert_eq!(a.is_none_operator(), true);
-        a.set_operator(Operation::ADD);
+        a.set_operator('+');
         assert_eq!(a.operator, Operation::ADD);
         assert_eq!(a.is_none_operator(), false);
         a.delete_operator();
         assert_eq!(a.operator, Operation::NONE);
+        a.set_operator('-');
+        assert_eq!(a.operator, Operation::SUB);
+        a.delete_operator();
+        a.set_operator('*');
+        assert_eq!(a.operator, Operation::MUL);
+        a.delete_operator();
+        a.set_operator('/');
+        assert_eq!(a.operator, Operation::DIV);
     }
     #[rstest]
     fn test_execute(fixture: RPN) {
         let mut a = fixture.clone();
-        a.set_operator(Operation::ADD);
+        a.set_operator('+');
         assert_eq!(a.execute(), Ok(8.0));
         assert_eq!(a.to_string(), "[1.0, 8.0]");
         assert_eq!(a.operator, Operation::NONE);
-        a.set_operator(Operation::SUB);
+        a.set_operator('-');
         assert_eq!(a.execute(), Ok(-7.0));
         assert_eq!(a.to_string(), "[-7.0]");
         a.push(9.0);
-        a.set_operator(Operation::MUL);
+        a.set_operator('*');
         assert_eq!(a.execute(), Ok(-63.0));
         assert_eq!(a.to_string(), "[-63.0]");
         a.push(-3.0);
         assert_eq!(a.to_string(), "[-63.0, -3.0]");
-        a.set_operator(Operation::DIV);
+        a.set_operator('/');
         assert_eq!(a.execute(), Ok(21.0));
         assert_eq!(a.to_string(), "[21.0]");
-        assert_eq!(a.execute(), Err("Not set operator".to_string()));
-        a.set_operator(Operation::ADD);
+        a.set_operator('+');
         assert_eq!(a.execute(), Err("Stack is shortage".to_string()));
+    }
+    #[rstest]
+    #[should_panic]
+    fn test_panic(fixture: RPN) {
+        let mut a = fixture.clone();
+        a.set_operator('&');
+        a.set_operator('+');
+        a.set_operator('+');
     }
 }
